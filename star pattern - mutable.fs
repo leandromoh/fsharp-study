5a7901615d9682b2f61c91b151d6c2fc62cc35df
Leandro Fernandes <leandromoh@hotmail.com>
2022-03-27 12:09:56 -0300

    Update star pattern - mutable.fs

--- star pattern - mutable.fs
+++ star pattern - mutable.fs
@@ -19,13 +19,13 @@ let printChar c times =
     for _ in 1 .. times do
         printf "%c" c
 
-let printLine times spaceStep starStep =
+let printLine times spaceInc starInc =
     for _ in 1 .. times do
         printChar ' ' spaces
         printChar '*' star
         printfn ""
-        spaces <- spaces + spaceStep
-        star <- star + starStep
+        spaces <- spaces + spaceInc
+        star <- star + starInc
 
 [<EntryPoint>]
 let main args =
